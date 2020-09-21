3755f65d10c49c6edf4d866d29b0858e5a4fc108
findmunish <findmunish@gmail.com>
2020-09-21 23:48:19 +0530

    reveal password functionality implemented

--- src/app/show-registered-users/show-registered-users.component.ts
+++ src/app/show-registered-users/show-registered-users.component.ts
@@ -8,9 +8,21 @@ import { REGISTERED_USERS } from '../registeredUsers'
 })
 export class ShowRegisteredUsersComponent implements OnInit {
 
-  users: any = REGISTERED_USERS
+  users: any = []
   constructor() { }
-
+  SHOW_PASSWORD = 'Reveal Password';
+  HIDE_PASSWORD = 'Hide Password';
   ngOnInit() {
+    for(let i = 0; i < REGISTERED_USERS.length; i++) {
+      let {username, password, label} = {label:this.SHOW_PASSWORD, ...REGISTERED_USERS[i]}; 
+      this.users.push({username, password, label})
+    }
+  }
+  toggleButtonDisplay(user) {
+    const index = this.users.findIndex(u => u.username === user.username && u.password === user.password);
+    if(index != -1) {
+      //this.users[index].label = (user.label.toLowerCase() === this.SHOW_PASSWORD.toLowerCase() ? user.password : this.LABEL);
+      this.users[index].label = this.users[index].label === this.SHOW_PASSWORD ? this.HIDE_PASSWORD : this.SHOW_PASSWORD;
+    }
   }
 }
