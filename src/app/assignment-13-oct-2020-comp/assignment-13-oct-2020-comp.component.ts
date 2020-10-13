f97c3b4b294f9b7bc99bfae822ac693bb032a2da
findmunish <findmunish@gmail.com>
2020-10-13 19:48:40 +0530

    banking r&d

--- src/app/assignment-13-oct-2020-comp/assignment-13-oct-2020-comp.component.ts
+++ src/app/assignment-13-oct-2020-comp/assignment-13-oct-2020-comp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-assignment-13-oct-2020-comp',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assignment-13-oct-2020-comp.component.css']
 })
 export class Assignment13Oct2020CompComponent implements OnInit {
-
-  constructor() { }
-
+  banks: any = [];
+  constructor(private httpClient: HttpClient) { }
   ngOnInit() {
+    // const banksLink = 'https://tin.tin.nsdl.com/tan/Bank.html';
+    // const banksLink = 'https://www.npci.org.in/national-automated-clearing-live-members-1';
+    const banksLink = 'https://github.com/razorpay/ifsc/releases/IFSC-list.json'
+    this.httpClient.get(banksLink).subscribe((bankRecords: any) => {
+      this.banks = bankRecords;
+    });
   }
-
 }
